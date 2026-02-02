a9dae981a82c6a6543e20dfb5e3bfca7a3a39b5b
programmermosharof <mosharof.dev@gmail.com>
2026-02-02 23:05:23 +0600

    math js

---
+++ math.js
@@ -0,0 +1,19 @@
+const min = Math.min(45, 21, 65, 99, 126, 5, -99);
+console.log(min);
+
+const max = Math.max(20, 222, 555, 60, 43, 65,);
+console.log(max);
+
+// console.log(Math.abc(4-2));
+
+console.log(Math.round(2.25));
+console.log(Math.round(2.55));
+console.log(Math.round(2.85));
+console.log(Math.floor(3.99));
+console.log(Math.ceil(5.25));
+
+console.log(Math.random());
+
+const rand = Math.random()*10;
+
+console.log(rand);
